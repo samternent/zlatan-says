46c40b90413eb0ff852c761e43fa6da2ae4c72dd
sam <sam.ternent@gmail.com>
2015-04-19 17:25:03 +0100

    Updates index port

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get('/style', function(req, res) {
 });
 
 
-app.listen(3000, function() {
-  console.log("Working on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
